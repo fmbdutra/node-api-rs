4074ba529b1c0eb25dc7f7cde540af3b93236151
Fabiano Matheus Bittencourt Dutra <000164@queroquero.local>
2019-08-06 16:43:51 -0300

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
+
 //Iniciando app
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 //Inicializando banco
 mongoose.connection('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
@@ -11,6 +15,6 @@ mongoose.connection('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true}
 require('./src/models');
 
 app.use('/api', require('.src/routes'));
-app.use(express.json());
+
 
 app.listen(3001);   
